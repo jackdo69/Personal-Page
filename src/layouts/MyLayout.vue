7ea62de158b4bb87e895923df605b4f4f4c6323f
jackdo69 <jackie.duc.do@gmail.com>
2019-12-04 16:00:59 +1100

    projects one

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -3,36 +3,42 @@
     <q-header bordered class="bg-white text-primary">
       <q-toolbar>
         <div class="q-pa-sm full-width row">
-
-          <div class="col-6">
-            <q-avatar class="q-ma-sm" size="60px">
+          <div class="col-6 relative-position">
+            <q-avatar class="q-pb-md absolute-center" size="60px">
               <img src="https://i.imgur.com/4LD0lMn.jpg" />
             </q-avatar>
           </div>
 
           <div class="col-6" id="line-menu">
-            <q-btn class="q-ma-sm" color="teal-4" dense flat label="About" />
+            <q-tabs v-model="tab" class="text-teal">
+              <a href="/#welcome-section"><q-tab name="mails" icon="assignment_ind" label="About" /></a>
+              <a href="/#projects"><q-tab name="alarms" icon="toys" label="Projects" /></a>
+              <a href="/#contact"><q-tab name="movies" icon="question_answer" label="Contact" /></a>
+              
+            </q-tabs>
 
-            <q-btn class="q-ma-sm" color="teal-4" dense flat label="Personal projects" />
+            <!-- <q-btn class="q-ma-sm" color="teal-4" dense flat label="About" />
 
-            <q-btn class="q-ma-sm" color="teal-4" dense flat label="Contact" />
+            <q-btn class="q-ma-sm" color="teal-4" dense flat label="Projects" />
+
+            <q-btn class="q-ma-sm" color="teal-4" dense flat label="Contact" />-->
           </div>
-          <div class="col-6" id="drop-down-menu" >
+          <div class="col-6" id="drop-down-menu">
             <q-btn color="teal-4" icon="dehaze" class="float-right">
-        <q-menu>
-          <q-list style="min-width: 100px">
-            <q-item clickable v-close-popup>
-              <q-item-section>About</q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup>
-              <q-item-section>Personal Project</q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup>
-              <q-item-section>Contact</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
+              <q-menu>
+                <q-list style="min-width: 100px">
+                  <q-item clickable v-close-popup>
+                    <q-item-section>About</q-item-section>
+                  </q-item>
+                  <q-item clickable v-close-popup>
+                    <q-item-section>Projects</q-item-section>
+                  </q-item>
+                  <q-item clickable v-close-popup>
+                    <q-item-section>Contact</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </q-btn>
           </div>
         </div>
       </q-toolbar>
@@ -50,8 +56,13 @@ export default {
 
   data() {
     return {
-      leftDrawerOpen: false
+      tab: 'mails'
     };
+  },
+  methods: {
+    toWelcome() {
+
+    }
   }
 };
 </script>
@@ -59,13 +70,18 @@ export default {
 <style>
 @media screen and (min-width: 768px) {
   #drop-down-menu {
-    display: none
+    display: none;
   }
 }
 
 @media screen and (max-width: 768px) {
   #line-menu {
-    display: none
+    display: none;
   }
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
 }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,226 @@
 <template>
   <q-page class="flex">
-    <div class="q-pa-xl full-width">
+    <div class="q-pa-xl">
       <div class="col q-mb-lg">
-        <h4>Hi, I am Jackie</h4>
-        <p>
-          I am a junior web developer, this is my personal webpage. Stay tune, content is comming...
-        </p>
+        <!-- START WELCOME SECTION -->
+
+<section id="welcome-section" class="welcome-section">
+  <h4>Hi, My name is Jackie</h4>
+  <p>I am a junior web developer, welcome to my page</p>
+</section>
+
+<!-- END WELCOME SECTION -->
+
+<!-- START PROJECTS SECTION -->
+
+<section id="projects" class="projects-section">
+  <h4>Here are some of my projects</h4>
+  <!-- <div class="projects-grid">
+    <a
+      href="http://128.199.196.65"
+      target="_blank"
+      class="project project-tile"
+    >
+      <img
+        class="project-image"
+        src="https://i.imgur.com/91s3aRT.png"
+        alt="project"
+      />
+      <p class="project-title">
+        <span class="code"></span>
+        Post-notes App
+        <span class="code"></span>
+      </p>
+    </a>
+
+    <a
+      href="http://128.199.196.65"
+      target="_blank"
+      class="project project-tile"
+    >
+      <img
+        class="project-image"
+        src="https://i.imgur.com/91s3aRT.png"
+        alt="project"
+      />
+      <p class="project-title">
+        <span class="code"></span>
+        Business Website
+        <span class="code"></span>
+      </p>
+    </a>
+
+    <a
+      href="http://128.199.196.65"
+      target="_blank"
+      class="project project-tile"
+    >
+      <img
+        class="project-image"
+        src="https://i.imgur.com/91s3aRT.png"
+        alt="project"
+      />
+      <p class="project-title">
+        <span class="code"></span>
+        Calculator App
+        <span class="code"></span>
+      </p>
+    </a>
+
+    <a
+      href="http://128.199.196.65"
+      target="_blank"
+      class="project project-tile"
+    >
+      <img
+        class="project-image"
+        src="https://i.imgur.com/91s3aRT.png"
+        alt="project"
+      />
+      <p class="project-title">
+        <span class="code"></span>
+        Drum Machine App
+        <span class="code"></span>
+      </p>
+    </a>
+
+    <a
+      href="http://128.199.196.65"
+      target="_blank"
+      class="project project-tile"
+    >
+      <img
+        class="project-image"
+        src="https://i.imgur.com/91s3aRT.png"
+        alt="project"
+      />
+      <p class="project-title">
+        <span class="code"></span>
+        Random Quote App
+        <span class="code"></span>
+      </p>
+    </a>
+
+    <a
+      href="http://128.199.196.65"
+      target="_blank"
+      class="project project-tile"
+    >
+      <img
+        class="project-image"
+        src="https://i.imgur.com/91s3aRT.png"
+        alt="project"
+      />
+      <p class="project-title">
+        <span class="code"></span>
+        Timer App
+        <span class="code"></span>
+      </p>
+    </a>
+
+    
+  </div> -->
+  <div class="row justify-center">
+
+    <div class="col-xs-10 col-sm-5 col-md-3 q-pa-md">
+      <q-card class="my-card">
+      <img src="https://i.imgur.com/Gbw8Caj.png">
+      <q-card-section>
+        <div class="text-h6">[Firebase-VueJS] Post-it Notes</div>  
+      </q-card-section>
+      <q-card-actions align="around">
+        <q-btn color="secondary" icon-right="img:https://i.imgur.com/gA2DgP3.png" label="Code" />
+        <q-btn color="red" icon-right="img:https://i.imgur.com/fhlgfX6.png" label="Demo" />
+      </q-card-actions>
+    </q-card>
+    </div>
+
+    <div class="col-xs-10 col-sm-5 col-md-3 q-pa-md">
+      <q-card class="my-card">
+      <img src="https://i.imgur.com/lm1sJzB.png">
+      <q-card-section>
+        <div class="text-h6">[Wordpress] Business Website</div>  
+      </q-card-section>
+      <q-card-actions align="around">
+        <q-btn color="secondary" icon-right="img:https://i.imgur.com/gA2DgP3.png" label="Code" />
+        <q-btn color="red" icon-right="img:https://i.imgur.com/fhlgfX6.png" label="Demo" />
+      </q-card-actions>
+    </q-card>
+    </div>
+
+    <div class="col-xs-10 col-sm-5 col-md-3 q-pa-md">
+      <q-card class="my-card">
+      <img src="https://i.imgur.com/fKuhUOS.png">
+      <q-card-section>
+        <div class="text-h6">[FCC-ReactJS] Calculator</div>  
+      </q-card-section>
+      <q-card-actions align="around">
+        <q-btn color="secondary" icon-right="img:https://i.imgur.com/gA2DgP3.png" label="Code" />
+        <q-btn color="red" icon-right="img:https://i.imgur.com/fhlgfX6.png" label="Demo" />
+      </q-card-actions>
+    </q-card>
+    </div>
+    
+  </div>
+
+  <div class="row justify-center">
+
+    <div class="col-xs-10 col-sm-5 col-md-3 q-pa-md">
+      <q-card class="my-card">
+      <img src="https://i.imgur.com/c45p7xT.png">
+      <q-card-section>
+        <div class="text-h6">[FCC-ReactJS] Random Quote</div>  
+      </q-card-section>
+      <q-card-actions align="around">
+        <q-btn color="secondary" icon-right="img:https://i.imgur.com/gA2DgP3.png" label="Code" />
+        <q-btn color="red" icon-right="img:https://i.imgur.com/fhlgfX6.png" label="Demo" />
+      </q-card-actions>
+    </q-card>
+    </div>
+
+    <div class="col-xs-10 col-sm-5 col-md-3 q-pa-md">
+      <q-card class="my-card">
+      <img src="https://i.imgur.com/JOXdPer.png">
+      <q-card-section>
+        <div class="text-h6">[FCC-ReactJS] Drum Machine</div>  
+      </q-card-section>
+      <q-card-actions align="around">
+        <q-btn color="secondary" icon-right="img:https://i.imgur.com/gA2DgP3.png" label="Code" />
+        <q-btn color="red" icon-right="img:https://i.imgur.com/fhlgfX6.png" label="Demo" />
+      </q-card-actions>
+    </q-card>
+    </div>
+
+    <div class="col-xs-10 col-sm-5 col-md-3 q-pa-md">
+      <q-card class="my-card">
+      <img src="https://i.imgur.com/jpPdKcQ.png">
+      <q-card-section>
+        <div class="text-h6">[FCC-ReactJS] Pomodoro Clock</div>  
+      </q-card-section>
+      <q-card-actions align="around">
+        <q-btn color="secondary" icon-right="img:https://i.imgur.com/gA2DgP3.png" label="Code" />
+        <q-btn color="red" icon-right="img:https://i.imgur.com/fhlgfX6.png" label="Demo" />
+      </q-card-actions>
+    </q-card>
+    </div>
+    
+  </div>
+  
+
+    
+  
+
+</section>
+
+<!-- END PROJECTS SECTION -->
+
+<!-- START CONTACT SECTION -->
+
+<section id="contact" class="contact-section">
+  <h4>Feel free to DMs on one of the following mediums:</h4>
+</section>
+
+<!-- END CONTACT SECTION -->
       </div>
       
       <hr />
@@ -18,3 +233,46 @@
 export default {  
 }
 </script>
+
+<style scoped>
+
+img {
+  display: block;
+  width: 100%;
+}
+
+/* Styling for welcome section */
+.welcome-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
+
+.projects-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+
+}
+
+.contact-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 100%;
+  height: 80vh;
+}
+/*End of welcome section */
+
+/*Styling for project section */
+
+/*End of project section */
+
+</style>
